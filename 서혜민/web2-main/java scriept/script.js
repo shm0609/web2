7f86cc4c012acd2066a0b7d5940197a9830cf18e
shm0609 <121483337+shm0609@users.noreply.github.com>
2023-01-03 19:30:52 +0900

    Add files via upload

---
+++ 서혜민/web2-main/java scriept/script.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){
+
+    $('.navi>li').mouseover(function(){
+        $(this).find('.submenu').stop().slideDown(500);
+    }).mouseout(function(){
+        $(this).find('.submenu').stop().slideup(500);
+    });
+});
